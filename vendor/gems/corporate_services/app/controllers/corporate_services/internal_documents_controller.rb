750a6445f7183b12f627eccc0f14976be42f7ff5
Les Nightingill <codehacker@comcast.net>
2015-04-10 20:31:19 -0700

    spike with jquery-file-upload basically working

--- vendor/gems/corporate_services/app/controllers/corporate_services/internal_documents_controller.rb
+++ vendor/gems/corporate_services/app/controllers/corporate_services/internal_documents_controller.rb
@@ -1,4 +1,9 @@
 class CorporateServices::InternalDocumentsController < ApplicationController
   def index
+    render :upload
+  end
+
+  def create
+    render :json => {:files => [{:name => "kablooie"}, {:name => "kablonka"}]}
   end
 end
--- vendor/gems/corporate_services/spec/features/internal_documents_spec.rb
+++ vendor/gems/corporate_services/spec/features/internal_documents_spec.rb
@@ -1,10 +1,22 @@
 require 'rails_helper'
+require 'login_helpers'
+require 'navigation_helpers'
 
 feature "internal document management" do
+  include LoggedInEnAdminUserHelper # sets up logged in admin user
+  include NavigationHelpers
   before do
-    
+    toggle_navigation_dropdown("Corporate Services")
+    select_dropdown_menu_item("Internal documents")
   end
+
   scenario "add a new document" do
-    expect("foo").to eq "bar"
+    expect(page_heading).to eq "Internal Documents"
+    expect(page_title).to eq "Internal Documents"
+    add_document_link.click
   end
+end
+
+def add_document_link
+  page.find('a#add_document')
 end
