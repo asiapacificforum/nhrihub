a181507c3d68e6e83d4d9dfe7fd5123527e9bb0b
Les Nightingill <codehacker@comcast.net>
2015-04-19 05:59:06 -0700

    comment out action_mailer_example initializer as it's being called

--- config/initializers/action_mailer_example.rb
+++ config/initializers/action_mailer_example.rb
@@ -1,13 +1,13 @@
-ActionMailer::Base.smtp_settings = {
-  :address => "mail.mydomain.com",
-  :port => 25,
-  :domain => "mydomain.com",
-  :authentication => :login,
-  :user_name => "user_name",
-  :password => "secret_password",
-  :enable_starttls_auto => false
-}
+#ActionMailer::Base.smtp_settings = {
+  #:address => "mail.mydomain.com",
+  #:port => 25,
+  #:domain => "mydomain.com",
+  #:authentication => :login,
+  #:user_name => "user_name",
+  #:password => "secret_password",
+  #:enable_starttls_auto => false
+#}
 
-Rails.application.config.action_mailer.raise_delivery_errors = true
-Rails.application.config.action_mailer.delivery_method = :smtp
+#Rails.application.config.action_mailer.raise_delivery_errors = true
+#Rails.application.config.action_mailer.delivery_method = :smtp
 
