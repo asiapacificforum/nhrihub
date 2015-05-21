c2bcd5d00ae157bffb870b3139aba94127afd442
Les Nightingill <codehacker@comcast.net>
2015-05-21 12:54:23 -0700

    enable vendored factories in production env

--- vendor/gems/corporate_services/config/initializers/internal_documents.rb
+++ vendor/gems/corporate_services/config/initializers/internal_documents.rb
@@ -1,4 +1,4 @@
-if Rails.env.development? || Rails.env.test?
+#if Rails.env.development? || Rails.env.test?
   factory_path = Rails.root.join('vendor','gems','corporate_services','spec','factories')
   FactoryGirl.definition_file_paths << factory_path
-end
+#end
