c1f24dc1de361a8a1e53985634c281d7934f0726
Les Nightingill <codehacker@comcast.net>
2015-05-27 18:37:40 -0700

    ensure production log is symlinked to shared directory

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,10 +23,22 @@ set :log_level, :debug
 set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/navigation.rb', 'lib/constants.rb', 'config/deploy/production.rb', 'app/assets/images/banner_logo.png', 'config/locales/site_specific/en.yml', 'config/locales/site_specific/fr.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml',
+                                                 'config/secrets.yml',
+                                                 'config/navigation.rb',
+                                                 'lib/constants.rb',
+                                                 'config/deploy/production.rb',
+                                                 'app/assets/images/banner_logo.png',
+                                                 'config/locales/site_specific/en.yml',
+                                                 'config/locales/site_specific/fr.yml')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log',
+                                               'tmp/pids',
+                                               'tmp/cache',
+                                               'tmp/sockets',
+                                               'vendor/bundle')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
