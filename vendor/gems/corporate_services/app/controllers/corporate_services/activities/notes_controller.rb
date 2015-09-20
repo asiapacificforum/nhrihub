63798e14db98470103916f93d0fbd697e02dbcec
Les Nightingill <codehacker@comcast.net>
2015-09-19 18:25:03 -0700

    notes controller inside modules inherits from the common notes controller in the main app

--- vendor/gems/corporate_services/app/controllers/corporate_services/activities/notes_controller.rb
+++ vendor/gems/corporate_services/app/controllers/corporate_services/activities/notes_controller.rb
@@ -1,36 +1,16 @@
-class CorporateServices::Activities::NotesController < ApplicationController
+require 'notes_controller'
+
+class CorporateServices::Activities::NotesController < NotesController
   def create
-    note = Note.new(note_params)
-    note.author = note.editor = current_user
-    if note.save
-      render :json => Note.where(:activity_id => note.activity_id), :status => 200
-    else
-      render :nothing => true, :status => 500
-    end
-  rescue => e # rescues strong parameter errors during development
-    puts e
-    render :nothing => true, :status => 500
+    super
   end
 
   def update
-    note = Note.find(params[:id])
-    note.editor = current_user
-    if note.update_attributes(note_params)
-      strategic_priorities = note.activity.outcome.planned_result.strategic_priority.strategic_plan.strategic_priorities
-      render :json => strategic_priorities, :status => 200
-    else
-      render :nothing => true, :status => 500
-    end
+    super
   end
 
   def destroy
-    note = Note.find(params[:id])
-    activity = note.activity
-    if note.destroy
-      render :json => activity.reload, :status => 200
-    else
-      render :nothing => true, :status => 500
-    end
+    super
   end
 
   private
