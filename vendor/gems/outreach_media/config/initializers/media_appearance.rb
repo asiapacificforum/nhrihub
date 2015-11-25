296012466d47fafa7e59afdd9f1241ccee7bb600
Les Nightingill <codehacker@comcast.net>
2015-11-25 10:51:42 -0800

    add media_appearance factories to production environment to support populating database

--- vendor/gems/outreach_media/config/initializers/media_appearance.rb
+++ vendor/gems/outreach_media/config/initializers/media_appearance.rb
@@ -1,5 +1,6 @@
+# factory needed in production in order to populate media appearance database
+factory_path = Rails.root.join('vendor','gems','outreach_media','spec','factories')
+FactoryGirl.definition_file_paths << factory_path
 if Rails.env.development? || Rails.env.test? || Rails.env.jstest?
-  factory_path = Rails.root.join('vendor','gems','outreach_media','spec','factories')
-  FactoryGirl.definition_file_paths << factory_path
   Rails.application.config.assets.paths << Rails.root.join('vendor', 'gems', 'outreach_media', 'spec', 'support', 'javascript')
 end
