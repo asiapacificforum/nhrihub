65ac72f46ec460f23a42d8a26059e34201dad52e
Les Nightingill <codehacker@comcast.net>
2015-11-25 10:55:41 -0800

    add authengine factories to production environment to support populating database

--- vendor/gems/authengine/config/initializers/factories.rb
+++ vendor/gems/authengine/config/initializers/factories.rb
@@ -1,4 +1,2 @@
-if Rails.env.development? || Rails.env.test? || Rails.env.jstest?
-  factory_path = Rails.root.join('vendor','gems','authengine','spec','factories')
-  FactoryGirl.definition_file_paths << factory_path
-end
+factory_path = Rails.root.join('vendor','gems','authengine','spec','factories')
+FactoryGirl.definition_file_paths << factory_path
