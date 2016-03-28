ed7a8fe9a475eb03da1de1d29c43c1684a0b3f57
Les Nightingill <codehacker@comcast.net>
2016-03-28 12:29:27 -0700

    small file rearrangements

---
+++ vendor/gems/nhri/spec/helpers/headings/offences_spec_helpers.rb
@@ -0,0 +1,6 @@
+require 'rspec/core/shared_context'
+
+module OffencesSpecHelpers
+  extend RSpec::Core::SharedContext
+end
+
---
+++ vendor/gems/nhri/spec/helpers/headings/offences_spec_setup_helpers.rb
@@ -0,0 +1,8 @@
+require 'rspec/core/shared_context'
+
+module OffencesSpecSetupHelpers
+  extend RSpec::Core::SharedContext
+  before do
+  end
+end
+
