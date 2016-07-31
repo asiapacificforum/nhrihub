9315c537ab3df6eef1b7c550a4ef7d6a0da2bc7f
Les Nightingill <codehacker@comcast.net>
2016-07-31 05:24:36 -0700

    wip on projects refactor branch

--- vendor/gems/projects/app/controllers/project_admin_controller.rb
+++ vendor/gems/projects/app/controllers/project_admin_controller.rb
@@ -3,7 +3,11 @@ class ProjectAdminController < ApplicationController
     @project_document_filetypes = ProjectDocument.permitted_filetypes
     @filetype = Filetype.new
     @filesize = ProjectDocument.maximum_filesize
-    @project_type = ProjectType.new
-    @project_types = Mandate.project_types.all.map(&:name)
+    @good_governance_project_type = ProjectType.new
+    @human_rights_project_type = ProjectType.new
+    @special_investigations_unit_project_type = ProjectType.new
+    @good_governance_project_types = Mandate.find_by(:key => 'good_governance').project_types.all.map(&:name)
+    @human_rights_project_types = Mandate.find_by(:key => 'human_rights').project_types.all.map(&:name)
+    @special_investigations_unit_project_types = Mandate.find_by(:key => 'special_investigations_unit').project_types.all.map(&:name)
   end
 end
