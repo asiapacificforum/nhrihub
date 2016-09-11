3a75e8c3bdda4af44cb29d488c3bee87a1e78cf9
Les Nightingill <codehacker@comcast.net>
2016-09-10 21:12:14 -0700

    fix errors occurring when mandates table is not configured

--- vendor/gems/projects/app/controllers/project_admin_controller.rb
+++ vendor/gems/projects/app/controllers/project_admin_controller.rb
@@ -6,8 +6,11 @@ class ProjectAdminController < ApplicationController
     @good_governance_project_type = ProjectType.new
     @human_rights_project_type = ProjectType.new
     @special_investigations_unit_project_type = ProjectType.new
-    @good_governance_project_types = Mandate.find_by(:key => 'good_governance').project_types.all.map(&:name)
-    @human_rights_project_types = Mandate.find_by(:key => 'human_rights').project_types.all.map(&:name)
-    @special_investigations_unit_project_types = Mandate.find_by(:key => 'special_investigations_unit').project_types.all.map(&:name)
+    good_governance_mandate = Mandate.find_by(:key => 'good_governance')
+    @good_governance_project_types = good_governance_mandate ? good_governance_mandate.project_types.all.map(&:name) : []
+    human_rights_mandate = Mandate.find_by(:key => 'human_rights')
+    @human_rights_project_types = human_rights_mandate ? human_rights_mandate.project_types.all.map(&:name) : []
+    special_investigations_unit_mandate = Mandate.find_by(:key => 'special_investigations_unit')
+    @special_investigations_unit_project_types = special_investigations_unit_mandate ? special_investigations_unit_mandate.project_types.all.map(&:name) : []
   end
 end
