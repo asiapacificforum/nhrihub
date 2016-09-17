a5a5bde1133b9bc0e9610c72be16d037f9121e33
Les Nightingill <codehacker@comcast.net>
2016-09-17 06:40:56 -0700

    add csp_reports, crude format, to dashboard

--- app/controllers/csp_reports_controller.rb
+++ app/controllers/csp_reports_controller.rb
@@ -2,14 +2,14 @@ class CspReportsController < ApplicationController
   skip_before_action :verify_authenticity_token, :check_permissions, only: [:create]
 
   def index
-    @reports = CspReport.all.order("created_at DESC")
+    @csp_reports = CspReport.all.order("created_at DESC")
+    render :partial => "index"
   end
 
   def create
     data = request.body.read
-
-    h = JSON.parse(data)["csp-report"]
-    report = CspReport.new(h)
+    attrs = JSON.parse(data)["csp-report"]
+    report = CspReport.new(attrs)
     report.save!
 
     head :ok
