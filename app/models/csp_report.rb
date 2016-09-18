eee7a27b10bf39027d7fadb3d1781d1d53a5a449
Les Nightingill <codehacker@comcast.net>
2016-09-17 20:18:38 -0700

    improved csp_report format for readability

--- app/models/csp_report.rb
+++ app/models/csp_report.rb
@@ -4,4 +4,20 @@ class CspReport < ActiveRecord::Base
   params.each do |param|
     alias_attribute "#{param}=", "#{param.underscore}="
   end
+
+  def violated_directive
+    vd = read_attribute(:violated_directive)
+    def vd.to_s
+      self.gsub(/nonce-(....).*?(....)'/,'nonce-\1...\2\'')
+    end
+    vd
+  end
+
+  def original_policy
+    op = read_attribute(:original_policy)
+    def op.to_s
+      self.gsub(/nonce-(....).*?(....)'/,'nonce-\1...\2\'')
+    end
+    op
+  end
 end
