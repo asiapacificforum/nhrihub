6e8d64ae07edda5cfbb721c025cab78311b5a63c
Les Nightingill <codehacker@comcast.net>
2016-11-30 21:46:03 -0800

    performance improvement in strategic plan by eager loading associations in depth

--- vendor/gems/corporate_services/app/controllers/corporate_services/strategic_plans_controller.rb
+++ vendor/gems/corporate_services/app/controllers/corporate_services/strategic_plans_controller.rb
@@ -3,9 +3,10 @@ class CorporateServices::StrategicPlansController < ApplicationController
     @strategic_plans = StrategicPlan.all_with_current.sort # for the select box options
     @strategic_priority = StrategicPriority.new
     if params[:id] == "current"
-      @strategic_plan = @strategic_plans.last # the current strategic plan
+      #@strategic_plan = @strategic_plans.last # the current strategic plan
+      @strategic_plan = StrategicPlan.where("strategic_plans.start_date >= ? and strategic_plans.start_date < ?",Date.today.advance(:years => -1),Date.today).includes(:strategic_priorities => {:planned_results => {:outcomes => {:activities => {:performance_indicators => [:media_appearances, :projects, :notes, :reminders]}}}}).first
     else
-      @strategic_plan = StrategicPlan.find(params[:id])
+      @strategic_plan = StrategicPlan.where(:id => params[:id]).includes(:strategic_priorities => {:planned_results => {:outcomes => {:activities => {:performance_indicators => [:media_appearances, :projects, :notes, :reminders]}}}}).first
     end
     @strategic_priorities = @strategic_plan.strategic_priorities
     respond_to do |format|
