64841466a1b202d901a01df6efd7f7c8514f740d
Les Nightingill <codehacker@comcast.net>
2016-12-01 21:14:04 -0800

    remove commented-out code

--- vendor/gems/strategic_plan_reporter/app/models/view_path.rb
+++ vendor/gems/strategic_plan_reporter/app/models/view_path.rb
@@ -2,12 +2,6 @@ require 'active_support/concern'
 module ViewPath
   extend ActiveSupport::Concern
 
-  #included do
-    #Root = StrategicPlanReporter::Engine.root
-    #TEMPLATE_PATH =  'app/views/strategic_plan_reporter/strategic_plan'
-    #TMP_DIR = 'tmp/strategic_plan_report'
-  #end
-
   def to_xml
     xml
   end
