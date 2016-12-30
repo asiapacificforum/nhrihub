41c00b408b733d3c67468db79f7034242a1af99d
Les Nightingill <codehacker@comcast.net>
2016-12-30 12:22:29 -0800

    move complaint basis models into the complaints module

--- vendor/gems/complaints/app/models/good_governance/complaint_basis.rb
+++ vendor/gems/complaints/app/models/good_governance/complaint_basis.rb
@@ -3,7 +3,7 @@ class GoodGovernance::ComplaintBasis < ComplaintBasis
   include NamedList
   # included as a reference, and as source for complaint_basis_factory.rb
   # actual names are user-configured via admin
-  DefaultNames = ["Private", "Unreasonable", "Unjust", "Failure to act", "Delayed action"]
+  DefaultNames = ["Private", "Unreasonable", "Unjust", "Failure to act", "Delayed action", "Formal", "Informal", "Out of Jurisdiction"]
   validates :name, :uniqueness => true
 
   def self.i18n_key
