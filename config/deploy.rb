f36c3b1831905eee287b0ec4236029a0228f36cf
Les Nightingill <codehacker@comcast.net>
2017-03-11 12:16:24 -0800

    small change to complaints.coffee to test asset recompilation

--- config/deploy.rb
+++ config/deploy.rb
@@ -64,10 +64,10 @@ namespace :deploy do
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+      #Here we can do anything such as:
+      #within release_path do
+        #execute :rake, 'tmp:cache:clear'
+      #end
     end
   end
 
