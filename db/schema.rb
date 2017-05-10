c184db50ebc45f433011b3a884529facb29f2afa
Les Nightingill <codehacker@comcast.net>
2017-05-09 21:21:27 -0700

    update git ref of get_back gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170507161533) do
+ActiveRecord::Schema.define(version: 20170509195716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -282,9 +282,10 @@ ActiveRecord::Schema.define(version: 20170507161533) do
     t.text     "archive_id"
     t.text     "checksum"
     t.text     "location"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
     t.json     "notification"
+    t.string   "archive_retrieval_job_id"
   end
 
   create_table "headings", force: :cascade do |t|
