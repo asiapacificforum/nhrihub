26e8335e455c3ea8e030f6e30dae3f2b06b667e4
Les Nightingill <codehacker@comcast.net>
2017-05-24 14:28:28 -0700

    add log level configuration for AwsLog (in get_back gem)

--- config/initializers/get_back.rb
+++ config/initializers/get_back.rb
@@ -3,4 +3,7 @@ if defined? GetBack
   GetBack::Config.setup do |config|
     config.include_dirs = [FileUploadLocation.join('store')]
   end
+
+  AwsLog.logger.level = 'debug' # possible values are: 'debug', 'info', 'warn', 'error', 'fatal'
+                                # 'debug' is the most liberal, 'fatal' the most restrictive
 end
