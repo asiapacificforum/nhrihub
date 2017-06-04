bb9f442b062f015d35d6bfef12d98fd6fa098950
Les Nightingill <codehacker@comcast.net>
2017-06-04 05:34:32 -0700

    update git ref of get_back gem

--- config/initializers/get_back.rb
+++ config/initializers/get_back.rb
@@ -1,7 +1,7 @@
 if defined? GetBack
   require 'get_back/config'
   GetBack::Config.setup do |config|
-    config.include_dirs = [FileUploadLocation.join('store')]
+    config.attached_files_directory = FileUploadLocation.join('store')
   end
 
   AwsLog.logger.level = 'debug' # possible values are: 'debug', 'info', 'warn', 'error', 'fatal'
