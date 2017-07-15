ff02422d8fd83fc75554a2eda892c580a49253b9
Les Nightingill <codehacker@comcast.net>
2017-07-15 08:55:09 -0700

    add rack-attack blocking for suspicious request patterns

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -82,6 +82,21 @@ class Rack::Attack
   #    ['']] # body
   # end
 
+  # Block suspicious requests for '/etc/password' or wordpress specific paths.
+  # After 3 blocked requests in 10 minutes, block all requests from that IP for 5 minutes.
+  Rack::Attack.blocklist('fail2ban pentesters') do |req|
+    # `filter` returns truthy value if request fails, or if it's from a previously banned IP
+    # so the request is blocked
+    Rack::Attack::Fail2Ban.filter("pentesters-#{req.ip}", :maxretry => 3, :findtime => 10.minutes, :bantime => 5.minutes) do
+      # The count for the IP is incremented if the return value is truthy
+      CGI.unescape(req.query_string) =~ %r{/etc/passwd} ||
+      req.path.include?('/etc/passwd') ||
+      req.path.include?('wp-admin') ||
+      req.path.include?('wp-login')
+
+    end
+  end
+
 end
 
 RackAttackLogger = Logger.new("log/rack-attack.log")
