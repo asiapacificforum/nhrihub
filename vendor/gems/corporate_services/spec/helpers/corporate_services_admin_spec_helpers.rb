36dc34986bda26b2f1e7df2b09a7bd156da8e0ef
Les Nightingill <codehacker@comcast.net>
2017-08-22 18:31:38 -0700

    strategic plan notes spec green under ractive 1.0.0 build9

--- vendor/gems/corporate_services/spec/helpers/corporate_services_admin_spec_helpers.rb
+++ vendor/gems/corporate_services/spec/helpers/corporate_services_admin_spec_helpers.rb
@@ -24,31 +24,4 @@ module CorporateServicesAdminSpecHelpers
   def delete_current_plan
     page.all('.delete_strategic_plan')[-1].click
   end
-  #def admin_page
-    #corporate_services_admin_path('en')
-  #end
-
-  #def filesize_context
-    #page.find('#internal_document_filesize')
-  #end
-
-  #def model
-    #InternalDocument
-  #end
-
-  #def filetypes_context
-    #page.find(filetypes_selector)
-  #end
-
-  #def filetypes_selector
-    #'#internal_document_filetypes'
-  #end
-
-  #def remove_add_delete_fileconfig_permissions
-    #ActionRole.
-      #joins(:action => :controller).
-      #where('actions.action_name' => ['create', 'destroy', 'update'],
-            #'controllers.controller_name' => ['corporate_services/internal_documents/filetypes','corporate_services/internal_documents/filesizes']).
-      #destroy_all
-  #end
 end
