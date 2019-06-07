6e92fa46fccaac0add97cc818d8f385ebcc605f9
Les Nightingill <codehacker@comcast.net>
2019-06-07 10:15:36 -0700

    change to environment.js for compatibility with webpacker new version

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,22 +3,22 @@ const { environment } = require('@rails/webpacker')
 //const { environment } = require('../../app/assets/javascripts/local_node_modules/@rails/webpacker')
 const webpack = require('webpack')
 
-environment.loaders.set('ractive_component', {
+environment.loaders.append('ractive_component', {
   test: /\.ractive\.html$/,
   use: 'ractive-bin-loader'
 })
 
-environment.loaders.set('ractive_pug_component', {
+environment.loaders.append('ractive_pug_component', {
   test: /\.ractive\.pug$/,
   use: ['ractive-bin-loader', 'pug-html-loader']
 })
 
-environment.loaders.set('locales', {
+environment.loaders.append('locales', {
  test: /\.ya?ml$/,
  loaders: ['json-loader', 'yaml-loader']
 })
 
-environment.plugins.set(
+environment.plugins.append(
   'Provide',
   new webpack.ProvidePlugin({
     UserInput : ['user_input_manager.coffee','UserInput'],
