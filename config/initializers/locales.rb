396d907deffb438054db546fb4153d34ea925743
Les Nightingill <codehacker@comcast.net>
2019-06-19 16:18:06 -0700

    link Rails app default_locale to I18n initialization

--- config/initializers/locales.rb
+++ config/initializers/locales.rb
@@ -2,6 +2,5 @@
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
   I18n.load_path += Dir.glob(Rails.root.join("config","locales","**","*.yml"))
   I18n.load_path += Dir.glob(Rails.root.join("vendor","gems","**","config","locales","**","*.yml"))
-  I18n.default_locale = ENV['RAILS_I18N_LOCALE'] || :en
+  Rails.application.config.i18n.default_locale = I18n.default_locale = ENV['RAILS_I18N_LOCALE'] || :en
   I18n.available_locales = [:en, :fr]
-
